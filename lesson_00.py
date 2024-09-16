39a6d406eabeeef8df3ac74c0e5e2c6fb7a17ea4
TrollFacedGamer <bwang@brooklynsteamcenter.org>
2024-09-16 10:53:45 -0400

    9/16/2024

---
+++ lesson_00.py
@@ -0,0 +1,34 @@
+#step 1
+print("Benny_Wang")
+
+#stap 2
+fav_show = "Murder_Drones"
+
+#step 3
+num_1 = 2
+num_2 = 0.2
+
+print(num_1 * num_2)
+
+fav_food = ["sausage", "french_fries", "chinese_fish_maw_soup"]
+
+age = input("what_is_your_age?")
+
+if int(age) > 15:
+    print("you_can_have_a_divers_license")
+else:
+    print("you_can_not_have_a_divers_license")
+
+#step 4
+name = input("what_is_your_name?")
+print(name)
+
+i = 0
+while i < 3:
+    print(fav_food[i])
+    i += 1
+
+x = 1
+y = 3
+while x == y:
+    print("Hello World")
