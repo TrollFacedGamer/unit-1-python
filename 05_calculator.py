cc69ff018e7fd5e455a63e11378654a3c04c0b48
Benny <158503969+TrollFacedGamer@users.noreply.github.com>
2024-09-25 21:56:56 -0400

    05_calculator update

--- 05_calculator.py
+++ 05_calculator.py
@@ -1,33 +1,83 @@
+#say welcome
 print("Welcome to the Calculatinator-inator 9000!")
 
-number_1 = int(input("Enter the first number:"))
-number_2 = int(input("Enter the second number:"))
+#ask for input
+input_1 = input("Enter the first number:")
+input_2 = input("Enter the second number:")
 
-print("Select operation:")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
-print("5. Floor Division")
-print("6. Exponential")
-print("7. Remainder")
+#check if input is purely numbers
+if input_1.isnumeric() and input_2.isnumeric():
+    #if so continue
 
-selected_operation = int(input("Enter choice:"))
+    #convert inputs to int
+    number_1 = int(input_1)
+    number_2 = int(input_2)
 
-if selected_operation == 1:
-    result = number_1+ number_2
-elif selected_operation == 2:
-    result = number_1 - number_2
-elif selected_operation == 3:
-    result = number_1 * number_2
-elif selected_operation == 4:
-    result = number_1 / number_2
-elif selected_operation == 5:
-    result = number_1 // number_2
-elif selected_operation == 6:
-    result = number_1 ** number_2
-elif selected_operation == 7:
-    result = number_1 % number_2
+    #list out operations
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Floor Division")
+    print("6. Exponential")
+    print("7. Remainder")
 
-print("Result:{}".format(result))
-    
+    #ask for operation
+    input_operation = input("Enter choice:")
+
+    #check if input is valid
+    if input_operation.isnumeric() and int(input_operation) <= 7:
+        #if so continue
+
+        #convert inputted operation into number
+        selected_operation = int(input_operation)
+
+        #match num with operation
+        if selected_operation == 1:
+            #Addition
+            result = number_1+ number_2
+            print("Result:{}".format(result))
+        elif selected_operation == 2:
+            #Subtraction
+            result = number_1 - number_2
+            print("Result:{}".format(result))
+        elif selected_operation == 3:
+            #Multiplication
+            result = number_1 * number_2
+            print("Result:{}".format(result))
+        elif selected_operation == 4:
+            #Division
+
+            #check if num_2 is =0
+            if number_2 == 0:
+                print("you can't divide by zero")
+            else:
+                #continue if not
+                result = number_1 / number_2
+                print("Result:{}".format(result))
+        elif selected_operation == 5:
+            #Floor Division
+            result = number_1 // number_2
+            print("Result:{}".format(result))
+        elif selected_operation == 6:
+            #Exponential
+            result = number_1 ** number_2
+            print("Result:{}".format(result))
+        elif selected_operation == 7:
+            #Remainder
+            
+            #check if num_2 is =0
+            if number_2 == 0:
+                print("you can't divide by zero")
+            else:
+                #continue if not
+                result = number_1 % number_2
+                print("Result:{}".format(result))
+    else:
+        #print error report if not
+        print("you can't input a letter or a unsupported operation")
+
+else:
+    #print error report if not
+    print("you inputted a letter instead of a number")
